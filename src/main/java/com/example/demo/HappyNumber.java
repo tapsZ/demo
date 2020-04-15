9fa64892a02e79e32bcb3fc80c5f950022d66eaa
Zireva <tapiwazireva@gmail.com>
2020-04-15 14:59:50 +0200

    LeetCode Practise

--- src/main/java/com/example/demo/HappyNumber.java
+++ src/main/java/com/example/demo/HappyNumber.java
@@ -6,6 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ *Write an algorithm to determine if a number n is "happy".
+ *
+ * A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+ *
+ * Return True if n is a happy number, and False if not.
+ *
+ * Example:
+ *
+ * Input: 19
+ * Output: true
+ * Explanation:
+ * 1^2 + 9^2 = 82
+ * 8^2 + 2^2 = 68
+ * 6^2 + 8^2 = 100
+ * 1^2 + 0^2 + 0^2 = 1
+ */
 public class HappyNumber {
 
     public static void main(String [] args){
@@ -15,7 +32,7 @@ public class HappyNumber {
 
     private static boolean isHappy(int n) {
         List<Integer> operatedNumber= new ArrayList<>();
-        boolean isHappy=false;
+        boolean isHappy;
         if (n == 1) {
             isHappy = true;
         } else {
@@ -25,7 +42,7 @@ public class HappyNumber {
     }
 
     private static boolean happy(int n, List<Integer> operatedNumber) {
-        boolean isHappy=false;
+        boolean isHappy;
         if (n == 1) {
             isHappy = true;
         } else if(operatedNumber.contains(n)){
